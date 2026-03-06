09149b4a3ba56250bab21e0cc18dc678efd346ac
Dean <106890173+Deanosaur666@users.noreply.github.com>
2026-03-06 15:42:06 -0800

    hehehe

--- titlefreq.c
+++ titlefreq.c
@@ -20,6 +20,12 @@ using the python program html-to-markdown
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <mpi.h>
+
 /*
 
 WORDS:
@@ -46,21 +52,38 @@ We don't care. We only count title cases and total words.
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <mpi.h>
+#define ALPHANUMERIC(c) ((c >= 'a' && c <= 'z') \
+                        || (c >= 'A' && c <= 'Z') \
+                        || (c >= '0' && c <= '9'))
 
-#define START_CAP 1
-#define START_LOWER 0
-#define NOT_WORD -1
+// could this character be the start of a word?
+#define START_WORD(c) (ALPHANUMERIC(c))
+// could this character be part of a word?
+#define IN_WORD(c) (ALPHANUMERIC(c) || c == '\'' || c == '.')
+
+/*
+
+The manager loads the file, and reads a fixed number of characters N
+every worker gets N characters to check
+
+*/
+
+typedef enum WORD_FLAGS {
+    UNDEFINED = 0,
+    NOT_WORD = 1,
+    
+    // these two flags are mutually exclusive
+    // one and only one must be set in any word
+    START_CAP = 1 << 1,
+    START_LOWER = 1 << 2,
+
+    // both are used to check for acronyms
+    DOUBLE_CAP = 1 << 3,
+    END_CAP = 1 << 4,
+} WORD_FLAGS;
 
 typedef struct Word {
-    int startType; // did it start with a capital letter?
-    // 0 = no, 1 = yes, -1 = not a word
-    int twocaps; // two caps in a row, for detecting acronyms
-    // boolish type, 0 = no, 1 = yes
+    int flags;
     int periodcount; // how many periods, for detecting acronyms
 } Word;
 
@@ -68,10 +91,43 @@ typedef struct Word {
 
 What do we do if a worker starts or ends partway into a word?
 We don't want to accidentally count extra words
-All books start with a hash symbol (#), so we always start
-in a "not in word" state
 
 */
 
-#define NOT_TITLE 0
-#define MAYBE_TITLE 1 // we don't know if it's an acronym yet
+/*
+
+Each worker returns:
+* word count
+* title case word count
+* start word (Word struct)
+* end word (Word struct)
+
+start word and end word exist so we can check for words split
+on section boundaries, so we don't overcount words
+
+if a worker starts or ends in a word, they don't add it to their word count
+the manager must decide if it's a whole word or a split word
+
+start and end words are stored in a queue or queues
+we record start and end words for sections sequentially
+
+once we have the end word, E, for section n, and the start word (S)
+for section n+1, we analyze them
+
+"having" them means neither is left undefined (at 0)
+
+if both are not words, we don't worry about it, and add nothing to any word count
+if both are words, it's one word, so we add one to word count
+    we then see if it's a valid title-case word:
+    E must start cap
+    E and S must not have double capitals
+    E period count + S period count < 2
+    if E ends cap, S must not start cap
+
+if only one of them is a word, we ignore the other, add one to word count
+    check the single word to see if it's a valid title-case word:
+    it must start cap
+    it must not have double capitols
+    period count must be < 2
+
+*/
